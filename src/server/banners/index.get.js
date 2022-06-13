30ef891ca428df28889d03bf5afa7deb49297768
hello-mayur <mayur.satpaise@publicissapient.com>
2022-06-13 15:38:02 +0530

    development first commit

---
+++ src/server/banners/index.get.js
@@ -0,0 +1,43 @@
+import offer1 from '../../static/images/offers/offer1.png';
+import offer2 from '../../static/images/offers/offer2.jpg';
+import offer3 from '../../static/images/offers/offer3.jpg';
+import offer4 from '../../static/images/offers/offer4.jpg';
+import offer5 from '../../static/images/offers/offer5.jpg';
+const BannerList=[
+  {
+    "bannerImageUrl":offer1,
+    "bannerImageAlt": "Independence Day Deal - 25% off on shampoo",
+    "isActive": true,
+    "order": 1,
+    "id": "5b6c38156cb7d770b7010ccc"
+  },
+  {
+    "bannerImageUrl": offer2,
+    "bannerImageAlt": "Independence Day Deal - Rs120 off on surf",
+    "isActive": true,
+    "order": 2,
+    "id": "5b6c38336cb7d770b7010ccd"
+  },
+  {
+    "bannerImageUrl": offer3,
+    "bannerImageAlt": "Independence Day Deal - Rs99 off on domex",
+    "isActive": true,
+    "order": 3,
+    "id": "5b6c38456cb7d770b7010cce"
+  },
+  {
+    "bannerImageUrl": offer4,
+    "bannerImageAlt": "Independence Day Deal - Rs99 off on bodywash",
+    "isActive": true,
+    "order": 4,
+    "id": "5b6c38576cb7d770b7010ccf"
+  },
+  {
+    "bannerImageUrl": offer5,
+    "bannerImageAlt": "Independence Day Deal - Rs70 off on tea",
+    "isActive": true,
+    "order": 5,
+    "id": "5b6c386b6cb7d770b7010cd0"
+  }
+]
+export default BannerList;
